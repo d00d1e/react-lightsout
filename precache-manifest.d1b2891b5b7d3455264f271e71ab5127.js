74dfff65c5c77fd8674ccc206e2c79139d6718b3
d00d1e <lee.julianay@gmail.com>
2021-01-29 13:48:53 -0800

    Updates

---
+++ precache-manifest.d1b2891b5b7d3455264f271e71ab5127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8df2a416d63b7beef44",
+    "url": "/react-lightsout/static/css/main.18df2da0.chunk.css"
+  },
+  {
+    "revision": "e8df2a416d63b7beef44",
+    "url": "/react-lightsout/static/js/main.e8df2a41.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/react-lightsout/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "b9d03965c830a0211208",
+    "url": "/react-lightsout/static/js/runtime~main.b9d03965.js"
+  },
+  {
+    "revision": "1d298220b60882e29db357d791e63a7b",
+    "url": "/react-lightsout/index.html"
+  }
+];
